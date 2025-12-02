2346fb18d20a7113b921041770cafb36cccaa1c5
Michiel Devaere <michiel.devaere@korazon.be>
2025-12-02 20:13:53 +0100

    Include TimeBasedQualityItem

---
+++ CSharp/GildedTros.App/Classes/TimeBasedQualityItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace GildedTros.App.ComplexClasses
+{
+    public class TimeBasedQualityItem : Item
+    {
+        public TimeBasedQualityItem(Item item)
+        {
+            Name = item.Name;
+            SellIn = item.SellIn;
+            Quality = item.Quality;
+        }
+        public List<QualityAdjustmentRule> QualityRules { get; set; } = new List<QualityAdjustmentRule>();
+        public int QualityImprovementPerDay { get; set; } = 1;
+
+    }
+
+    public class QualityAdjustmentRule
+    {
+        public int DaysThreshold { get; set; }
+        public int? QualityChangePerDay { get; set; }
+        public int? AbsoluteQuality { get; set; }
+    }
+}
