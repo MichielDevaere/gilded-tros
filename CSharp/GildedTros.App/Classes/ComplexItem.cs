07cff0f531e898be0beaff639fbe8f69be3f5bc0
Michiel Devaere <michiel.devaere@korazon.be>
2025-12-03 09:40:55 +0100

    Include ComplexItem

---
+++ CSharp/GildedTros.App/Classes/ComplexItem.cs
@@ -0,0 +1,28 @@
+﻿using GildedTros.App.Interfaces;
+
+namespace GildedTros.App.Classes
+{
+    public class ComplexItem : Item, IMaxQuality
+    {
+        public ComplexItem(Item item)
+        {
+            Name = item.Name;
+            SellIn = item.SellIn;
+            Quality = item.Quality;
+        }
+
+        private int? _maxQuality;
+        public int MaxQuality
+        {
+            get
+            {
+                if (!_maxQuality.HasValue)
+                {
+                    _maxQuality = Settings.GetMaxQuality(GetType().Name);
+                }
+                return _maxQuality.Value;
+            }
+            set => _maxQuality = value;
+        }
+    }
+}
